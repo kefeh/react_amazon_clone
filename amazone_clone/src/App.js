d69d56b2753c9435e23e729dfa1473ccfd688f39
kefeh <kefehcollins@gmail.com>
2020-08-16 11:05:54 +0100

    Adds the initial file setups and creat app

---
+++ amazone_clone/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
+          <Route path="/">
+            Home
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
